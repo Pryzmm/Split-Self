1df4e5a39e9eded58f1b8383e9aceeea40fa9fb6
roteroam <140432727+realphilamator@users.noreply.github.com>
2025-08-31 18:17:15 -0500

    Added Auto-Complete to commands (#53)

--- src/main/java/com/pryzmm/splitself/command/SplitSelfCommands.java
+++ src/main/java/com/pryzmm/splitself/command/SplitSelfCommands.java
@@ -1,7 +1,11 @@
 package com.pryzmm.splitself.command;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.suggestion.SuggestionProvider;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 import com.pryzmm.splitself.SplitSelf;
 import com.pryzmm.splitself.events.*;
 import com.pryzmm.splitself.screen.WarningScreen;
@@ -14,54 +18,113 @@ import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.Arrays;
+
 public class SplitSelfCommands {
+
+    // Suggestion provider for main subcommands
+    private static final SuggestionProvider<ServerCommandSource> MAIN_SUGGESTIONS =
+            (context, builder) -> {
+                String[] suggestions = {
+                        "information",
+                        "debugFullscreen",
+                        "debugToggleEvents",
+                        "debugSleepStage",
+                        "runEvent"
+                };
+
+                for (String suggestion : suggestions) {
+                    if (suggestion.toLowerCase().startsWith(builder.getRemaining().toLowerCase())) {
+                        builder.suggest(suggestion);
+                    }
+                }
+                return builder.buildFuture();
+            };
+
+    private static final SuggestionProvider<ServerCommandSource> EVENT_SUGGESTIONS =
+            (context, builder) -> {
+                if ("random".startsWith(builder.getRemaining().toLowerCase())) {
+                    builder.suggest("random");
+                }
+
+                for (EventManager.Events event : EventManager.Events.values()) {
+                    String eventName = event.name().toLowerCase();
+                    if (eventName.startsWith(builder.getRemaining().toLowerCase())) {
+                        builder.suggest(eventName);
+                    }
+                }
+                return builder.buildFuture();
+            };
+
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher, CommandRegistryAccess registryAccess, CommandManager.RegistrationEnvironment environment) {
         MinecraftClient client = MinecraftClient.getInstance();
+
         dispatcher.register(CommandManager.literal("splitself")
                 .executes(context -> {
                     context.getSource().sendFeedback(() -> Text.literal("<" + context.getSource().getName() + "> " + SplitSelf.translate("command.splitself.empty_command").getString()), false);
                     return 1;
                 })
-                .then(CommandManager.argument("text", StringArgumentType.word())
+                .then(CommandManager.literal("information")
                         .executes(context -> {
-                            String argument = StringArgumentType.getString(context, "text").toLowerCase();
-                            if (argument.equalsIgnoreCase("information")) {
-                                client.execute(() -> client.setScreen(new WarningScreen()));
-                            } else if (argument.equalsIgnoreCase("debugFullscreen")) {
-                                if (client.options.getFullscreen().getValue()) {
-                                    context.getSource().sendFeedback(() -> Text.literal("In fullscreen"), false);
-                                } else {
-                                    context.getSource().sendFeedback(() -> Text.literal("NOT in fullscreen"), false);
-                                }
-                            } else if (argument.equalsIgnoreCase("debugToggleEvents")) {
-                                DataTracker tracker = DataTracker.getServerState(client.getServer());
-                                tracker.setPlayerReadWarning(client.player.getUuid(), !tracker.getPlayerReadWarning(client.player.getUuid()));
-                                context.getSource().sendFeedback(() -> Text.literal(SplitSelf.translate("command.splitself.debug_toggle_warning", tracker.getPlayerReadWarning(client.player.getUuid())).getString()), false);
-                            } else if (argument.equalsIgnoreCase("debugSleepStage")) {
-                                DataTracker tracker = DataTracker.getServerState(client.getServer());
-                                context.getSource().sendFeedback(() -> Text.literal(String.valueOf(tracker.getPlayerSleepStage(client.player.getUuid()))), false);
+                            client.execute(() -> client.setScreen(new WarningScreen()));
+                            return 1;
+                        })
+                )
+                .then(CommandManager.literal("debugFullscreen")
+                        .executes(context -> {
+                            if (client.options.getFullscreen().getValue()) {
+                                context.getSource().sendFeedback(() -> Text.literal("In fullscreen"), false);
                             } else {
-                                context.getSource().sendFeedback(() -> Text.literal("<" + context.getSource().getName() + "> " + SplitSelf.translate("command.splitself.invalid_value").getString()), false);
+                                context.getSource().sendFeedback(() -> Text.literal("NOT in fullscreen"), false);
                             }
                             return 1;
                         })
-                        .then(CommandManager.argument("event", StringArgumentType.greedyString())
+                )
+                .then(CommandManager.literal("debugToggleEvents")
+                        .executes(context -> {
+                            DataTracker tracker = DataTracker.getServerState(client.getServer());
+                            tracker.setPlayerReadWarning(client.player.getUuid(), !tracker.getPlayerReadWarning(client.player.getUuid()));
+                            context.getSource().sendFeedback(() -> Text.literal(SplitSelf.translate("command.splitself.debug_toggle_warning", tracker.getPlayerReadWarning(client.player.getUuid())).getString()), false);
+                            return 1;
+                        })
+                )
+                .then(CommandManager.literal("debugSleepStage")
+                        .executes(context -> {
+                            DataTracker tracker = DataTracker.getServerState(client.getServer());
+                            context.getSource().sendFeedback(() -> Text.literal(String.valueOf(tracker.getPlayerSleepStage(client.player.getUuid()))), false);
+                            return 1;
+                        })
+                        // Debug Sleep Stage subcommand - set stage
+                        .then(CommandManager.argument("stage", IntegerArgumentType.integer(0))
                                 .executes(context -> {
-                                    String firstArg = StringArgumentType.getString(context, "text").toLowerCase();
-                                    String secondArg = StringArgumentType.getString(context, "event").toLowerCase();
+                                    int stage = IntegerArgumentType.getInteger(context, "stage");
                                     ServerWorld world = context.getSource().getWorld();
                                     PlayerEntity player = context.getSource().getPlayer();
-                                    if (firstArg.equalsIgnoreCase("debugSleepStage") && SplitSelf.isNumeric(secondArg)) {
-                                        DataTracker tracker = DataTracker.getServerState(world.getServer());
-                                        tracker.setPlayerSleepStage(player.getUuid(), Integer.parseInt(secondArg));
-                                        context.getSource().sendFeedback(() -> Text.literal(String.valueOf(tracker.getPlayerSleepStage(client.player.getUuid()))), false);
-                                    } else if (firstArg.equalsIgnoreCase("runevent") && secondArg.equalsIgnoreCase("random")) {
+                                    DataTracker tracker = DataTracker.getServerState(world.getServer());
+                                    tracker.setPlayerSleepStage(player.getUuid(), stage);
+                                    context.getSource().sendFeedback(() -> Text.literal(String.valueOf(tracker.getPlayerSleepStage(client.player.getUuid()))), false);
+                                    return 1;
+                                })
+                        )
+                )
+                .then(CommandManager.literal("runEvent")
+                        .then(CommandManager.argument("event", StringArgumentType.word())
+                                .suggests(EVENT_SUGGESTIONS)
+                                .executes(context -> {
+                                    String eventArg = StringArgumentType.getString(context, "event").toLowerCase();
+                                    ServerWorld world = context.getSource().getWorld();
+                                    PlayerEntity player = context.getSource().getPlayer();
+
+                                    if (eventArg.equalsIgnoreCase("random")) {
                                         EventManager.triggerRandomEvent(world, player, null, true);
-                                    } else try {
-                                        EventManager.Events event = EventManager.Events.valueOf(secondArg.toUpperCase());
-                                        EventManager.triggerRandomEvent(world, player, event, true);
-                                    } catch (IllegalArgumentException e) {
-                                        context.getSource().sendFeedback(() -> Text.literal("<" + context.getSource().getName() + "> " + SplitSelf.translate("command.splitself.invalid_value").getString()), false);
+                                    } else {
+                                        try {
+                                            EventManager.Events event = EventManager.Events.valueOf(eventArg.toUpperCase());
+                                            EventManager.triggerRandomEvent(world, player, event, true);
+                                        } catch (IllegalArgumentException e) {
+                                            context.getSource().sendFeedback(() -> Text.literal("<" + context.getSource().getName() + "> " + SplitSelf.translate("command.splitself.invalid_value").getString()), false);
+                                        }
                                     }
                                     return 1;
                                 })
